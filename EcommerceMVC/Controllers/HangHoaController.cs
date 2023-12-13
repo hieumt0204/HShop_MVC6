77dcdc1b33b8a43a47f2f43ea85c9c1a4448354c
hieumthe1638431 <hieumthe163841@fpt.edu.vn>
2023-12-13 19:23:07 +0700

    First code

---
+++ EcommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,71 @@
+﻿using EcommerceMVC.Data;
+using EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly Hshop2023Context db;
+
+        public HangHoaController(Hshop2023Context context) 
+        {
+            db = context;
+        }          
+        public IActionResult Index(int? loai)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+            if(loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM()
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+            if(query != null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM()
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var data = db.HangHoas.SingleOrDefault(p => p.MaHh == id);
+            if(data == null)
+            {
+                TempData["Message"] = $"Không tìm thấy sản phẩm có mã {id}";
+                return Redirect("/404");
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+
+    }
+}
